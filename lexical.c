88260c0b26f98b5b5d3efc0918ce975b1a06ea00
Chomi <chomo.tomas@gmail.com>
2014-10-19 18:45:16 +0200

    Lexical

---
+++ lexical.c
@@ -0,0 +1,357 @@
+// IFJ14, projekt do predmetu IFJ pre 2BIT 2014/2015 //
+/////// Autor: Jan Profant
+///////        Filip Badan
+///////        Michal Chomo
+///////        Tomas Chomo
+///////        Findo
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "lexical.h"
+#include "lexstring.h"
+
+//retazec klucovych slov
+const char key_words [] = {",begin,boolean,do,else,end,false,find,forward,function,if,integer,readln,real,sort,string,then,true,var,while,write,"};
+//
+
+TOKEN *get_token ()
+{
+        int i;
+
+        if (isspace(c)!=0)                                                                      // preskoc medzery
+        {
+            while (isspace(c)!=0 && c!=EOF) c=fgetc(fp);
+        }
+        if (c==EOF) return NULL;
+        if (c=='{')                                                                             //preskakovanie komentarov
+        {
+            while ((c=fgetc(fp))!='}' && (c!=EOF));
+        }
+
+        i=0;
+        switch (c)
+        {
+            case 'A'...'Z':
+                case 'a'...'z':
+                    case '_':                                                                   // konecny automat pre identifikator
+                    {
+                        if (i==0)
+                        {
+                            token->mem=first_allocation ();
+                            token->mem=string_implementation(c,i,token->mem);
+                        }
+                        c=fgetc(fp);
+                        while ((c>='A' && c<='Z') || (c>='a' && c<='z') || (c=='_') || (c>='0' && c<='9'))
+                        {
+                            i++;
+                            token->mem=string_implementation(c,i,token->mem);
+                            c=fgetc(fp);
+                        }
+                        token->mem=string_implementation('\0',i+1,token->mem);
+                        if (is_key_word(token->mem)==0) token->identity=1;
+                        else token->identity=6;
+                        return token;
+                    }
+
+            case '0'...'9':                                                                     // je to cislo
+            {
+                if (i==0)
+                {
+                    token->mem=first_allocation ();
+                    token->mem=string_implementation(c,i,token->mem);
+                }
+                c=fgetc(fp);
+                while (c>='0' && c<='9')
+                {
+                    i++;
+                    token->mem=string_implementation(c,i,token->mem);
+                    c=fgetc(fp);
+                }
+                if (c=='.')
+                {
+                    i++;
+                    token->mem=string_implementation(c,i,token->mem);
+                    c=fgetc(fp);
+                    while ((c)>='0' && c<='9')
+                    {
+                        i++;
+                        token->mem=string_implementation(c,i,token->mem);
+                        c=fgetc(fp);
+                    }
+                    switch (c)
+                    {
+                        case 'e':
+                        case 'E':
+                        {
+                            i++;
+                            token->mem=string_implementation(c,i,token->mem);
+                            c=fgetc(fp);
+                            while ((c)>='0' && c<='9')
+                            {
+                                i++;
+                                token->mem=string_implementation(c,i,token->mem);
+                                c=fgetc(fp);
+                            }
+                            break;
+                        }
+                        case '+':
+                        case '-':
+                        {
+                            i++;
+                            token->mem=string_implementation(c,i,token->mem);
+                            c=fgetc(fp);
+                            if (c=='e' || c=='E')
+                            {
+                                c=fgetc(fp);
+                                while ((c)>='0' && c<='9')
+                                {
+                                    i++;
+                                    token->mem=string_implementation(c,i,token->mem);
+                                    c=fgetc(fp);
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+                if (c=='e' || c=='E')
+                {
+                    i++;
+                    token->mem=string_implementation(c,i,token->mem);
+                    c=fgetc(fp);
+                    while ((c)>='0' && c<='9')
+                    {
+                        i++;
+                        token->mem=string_implementation(c,i,token->mem);
+                        c=fgetc(fp);
+                    }
+                }
+                else if ((strstr(token->mem,"."))==NULL)
+                {
+                    token->mem=string_implementation('\0',i+1,token->mem);
+                    token->identity = 2;
+                    return token;
+                }
+                token->mem=string_implementation('\0',i+1,token->mem);
+                token->identity = 3;
+                return token;
+            }
+
+            case '+':                                                                                   // je to numericky operator
+                case '-':
+                    case '*':
+                        case '/':
+                        {
+                            if (i==0)
+                            {
+                                token->mem=first_allocation ();
+                                token->mem=string_implementation(c,i,token->mem);
+                            }
+                            token->mem=string_implementation('\0',i+1,token->mem);
+                            c=fgetc(fp);
+                            token->identity = 7;
+                            return token;
+                        }
+
+            case ':':                                                                                   // je to :, moze sa jednat o priradenie
+            {
+                if (i==0)
+                {
+                    token->mem=first_allocation ();
+                    token->mem=string_implementation(c,i,token->mem);
+                }
+                c=fgetc(fp);
+                if (c=='=')                                                                             // jedna sa o priradenie
+                {
+                    i++;
+                    token->mem=string_implementation(c,i,token->mem);
+                    c=fgetc(fp);
+                }
+                token->mem=string_implementation('\0',i+1,token->mem);
+                token->identity = 7;
+                return token;
+            }
+
+            case ';':
+            {
+                if (i==0)
+                {
+                    token->mem=first_allocation ();
+                    token->mem=string_implementation(c,i,token->mem);
+                }
+                token->mem=string_implementation('\0',i+1,token->mem);
+                c=fgetc(fp);
+                token->identity = 7;
+                return token;
+            }
+
+            case '>':
+            {
+                if (i==0)
+                {
+                    token->mem=first_allocation ();
+                    token->mem=string_implementation(c,i,token->mem);
+                }
+                c=fgetc(fp);
+                if (c=='=')
+                {
+                    i++;
+                    token->mem=string_implementation(c,i,token->mem);
+                }
+                token->mem=string_implementation('\0',i+1,token->mem);
+                token->identity = 7;
+                return token;
+            }
+
+            case '<':
+            {
+                if (i==0)
+                {
+                    token->mem=first_allocation ();
+                    token->mem=string_implementation(c,i,token->mem);
+                }
+                c=fgetc(fp);
+                if (c=='>')
+                {
+                    i++;
+                    token->mem=string_implementation(c,i,token->mem);
+                }
+                if (c=='=')
+                {
+                    i++;
+                    token->mem=string_implementation(c,i,token->mem);
+                }
+                token->mem=string_implementation('\0',i+1,token->mem);
+                token->identity = 7;
+                return token;
+            }
+
+            case '(':
+            case ')':
+            {
+                if (i==0)
+                {
+                    token->mem=first_allocation ();
+                    token->mem=string_implementation(c,i,token->mem);
+                }
+                token->mem=string_implementation('\0',i+1,token->mem);
+                c=fgetc(fp);
+                token->identity = 7;
+                return token;
+            }
+
+            case '\'':
+            {
+                if (i==0)
+                {
+                    token->mem=first_allocation ();
+                    token->mem=string_implementation(c,i,token->mem);
+                }
+                c=fgetc(fp);
+                while (c!='\'' && c!=EOF)
+                {
+                    i++;
+                    token->mem=string_implementation(c,i,token->mem);
+                    c=fgetc(fp);
+                }
+                if (c=='\'')
+                {
+                    i++;
+                    token->mem=string_implementation(c,i,token->mem);
+                }
+                else return NULL;
+                c=fgetc(fp);
+                token->mem=string_implementation('\0',i+1,token->mem);
+                token->identity = 5;
+                return token;
+            }
+
+            case '.':
+            {
+                if (i==0)
+                {
+                    token->mem=first_allocation ();
+                    token->mem=string_implementation(c,i,token->mem);
+                    c=fgetc(fp);
+                    token->mem=string_implementation('\0',i+1,token->mem);
+                    token->identity = 7;
+                    return token;
+                }
+            }
+        }
+
+    return NULL;
+}
+
+int is_key_word (char *key)
+{
+    unsigned int p=0;
+    for (unsigned int i=1;i<=strlen(key_words);i++)
+    {
+        if (key[0]==key_words[i] && key_words[i-1]==',')
+        {
+            p=0;
+            int j=1;
+            while (key_words[i]!=',')
+            {
+                i++;
+                if (key_words[i]==key[j])
+                {
+                    p++;
+                    j++;
+                }
+                else break;
+            }
+            if (p+1==(strlen(key)) && (j-p)==1) return 1;                                               // vrat 1 ak si nasiel identifikator v klucovych slovach
+        }
+    }
+    return 0;                                                                                           // nenasiel si nic :(
+}
+
+int open_file (char *filename,int argc)
+{
+    if (argc!=2)
+    {
+        fprintf (stderr,"Argument error\n");
+        return 99;
+    }
+    fp = fopen (filename,"r");
+    if (fp==NULL)
+    {
+        fprintf (stderr,"File not found\n");
+        exit (99);
+    }
+    return 0;
+}
+
+void close_file ()
+{
+    fclose(fp);
+}
+
+int main (int argc, char **argv)
+{
+    open_file(argv[1],argc);
+    c=fgetc(fp);
+
+    token=malloc(sizeof(TOKEN));
+    if (token==NULL)
+    {
+        fprintf(stderr,"Allocation failed");
+        close_file();
+        exit (99);
+    }
+
+    TOKEN *lexem;
+    while ((lexem=get_token())!=NULL)
+    {
+        if (lexem!=NULL) token=lexem;
+        else break;
+        printf ("%s - %d\n",token->mem,token->identity);
+        free (token->mem);
+    }
+    free (token);
+    close_file (argv[1]);
+    return 0;
+}
---
+++ lexical.h
@@ -0,0 +1,50 @@
+// IFJ14, projekt do predmetu IFJ pre 2BIT 2014/2015 //
+/////// Autor: Jan Profant
+///////        Filip Badan
+///////        Michal Chomo
+///////        Tomas Chomo
+///////        Findo
+
+#ifndef _LEXICAL_H_
+#define _LEXICAL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+#define NATIVE_CHAR 10                                              //velkost retazca pre ukladanie
+
+
+
+FILE *fp;                                                           //subor
+int c;                                                              //nacitavany znak
+
+// identita oznacuje typ tokenu
+// 1 - identifikator
+// 2 - integer
+// 3 - real
+// 4 - boolean
+// 5 - string
+// 6 - klucove slovo
+// 7 - operator
+
+typedef enum TYP {identifikator = 1, integer, real, boolean, string, kluc_slovo, operator} identita;
+
+
+typedef struct token
+{
+    identita identity;                                              // prislusnost ku skupine
+    char *mem;                                                      // ukazovatel na retazec
+} TOKEN;
+
+
+
+TOKEN *token;                                                       // struktura tokenu
+
+TOKEN *get_token ();                                                // v ukazovateli vrat jeden token
+int open_file (char *filename,int argc);                            // otvor subor
+void close_file ();                                                 // zavri subor
+int is_key_word (char *key);                                        // zisti ci ide o klucove slovo
+
+#endif
