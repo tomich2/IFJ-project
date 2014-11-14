ce59fc4516bac261fb0814e683bd21b1577b2663
toudwort <toudwort@gmail.com>
2014-11-14 17:41:46 +0100

    Update main.c

--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 #include "lexstring.h"
 #include "error.h"
 #include "stack.h"
-
+#include "parser.h"
 
 int main (int argc, char **argv)
 {
@@ -26,36 +26,6 @@ int main (int argc, char **argv)
         Error(99);
     }
 
-    TOKEN *lexem;
-    while ((lexem=get_token())!=NULL)
-    {
-        if (lexem!=NULL) token=lexem;
-        else break;
-        printf ("%s - %d\n",token->mem,token->identity);
-        free (token->mem);
-    }
-    free (token);
-    close_file (argv[1]);
-    
-    /* test zasobnika ***************************** */
-     char s1[]="Ahoj";
-     char s2[]="Svet";
-
-     char *s;
-
-     Stack stack;
-     init(&stack, 255);
-     push(&stack,s1,-1);
-     printf("vrchol je %s\n", s=top(&stack));
-     pop(&stack);
-     if (S_empty(&stack)) printf("zasobnik je prazdny\n");
-     push(&stack,s1,strlen(s1));
-     push(&stack,s2,strlen(s2));
-     printf("vrchol je %s\n", s=top(&stack));
-     pop(&stack);
-     printf("vrchol je %s\n", s=top(&stack));
-     pop(&stack);
-     if (S_empty(&stack)) printf("zasobnik je prazdny\n");
-     /* test zasobnika ***************************** */
+   if(!top_down()) printf("subor je spravny\n");
     return 0;
 }
