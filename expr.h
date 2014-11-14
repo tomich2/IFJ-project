072c6982aad6ed71ab24458adc1ca4f856003a41
RPMcMurphy <8blink182@gmail.com>
2014-11-14 17:08:23 +0100

    Create expr.h

---
+++ expr.h
@@ -0,0 +1,21 @@
+#ifndef EXPR_H
+#define EXPR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "stack.h"
+#include "parser.h"
+typedef enum prec { R /* Right > */, L /* Left < */, E /* Equal = */, Q /* Error */ } action;
+
+typedef enum Operator { EPlus, EMinus, EMulti, EDiv, EEqual,
+			ELess, EGreater, ELorEq, EGorEq, EDiff,
+			EId, ETerm,  EComma, ELpar, ERpar, EEnd} oprs;
+
+oprs converttooprs(identita id);
+int GetRule(int a, int b);
+T_ParserItem *GetTerm(Stack *stack, bool hdl);
+int Reduction(Stack *stack);
+int ExprParse();
+#endif
