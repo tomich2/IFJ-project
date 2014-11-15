597f673825081a4c2c15a28b4143436f46c8fe59
Jamiroquai <jan.j.profant@gmail.com>
2014-11-15 14:30:35 +0100

    Update error.h

--- error.h
+++ error.h
@@ -9,4 +9,16 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+typedef enum ERR {  LEXICAL_ERR=1,
+                    SYNTAX_ERR,
+                    SEMANTIC_ERR,
+                    EXPRESSION_ERR,
+                    OTHER_SEM_ERR,
+                    INPUT_ERR,
+                    UNINICIALIZED_ERR,
+                    ZERO_DIV_ERR,
+                    AND_OTHERS_ERR,
+                    INTERN_INTERPRETATION_ERR=99
+                 }ERROR_MSG;
+
 void Error(int p);
