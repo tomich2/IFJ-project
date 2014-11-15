5369c7634b9b7a1b41dca9a3eb34429127a36e2c
Jamiroquai <jan.j.profant@gmail.com>
2014-11-15 20:39:11 +0100

    Update test0.sh

--- test0.sh
+++ test0.sh
@@ -1,75 +1,142 @@
 #!/bin/bash
 
-red='\e[0;32m';
+red='\e[0;31m';
 NC='\e[0m';
-green='\e[0;31m';
+green='\e[0;32m';
 
-if [ -d "$DIRECTORY" ]; 
+if [ -d "$DIRECTORY" ];
     then
         mkdir test0.outputs;
 fi
 
-./lexical > ./test0.outputs/nofile.output 2>&1;
+./parser > ./test0.outputs/nofile.output 2>&1;
 value=$?;
-if [ $value = 0 ];
+expected=99;
+if [ $value = $expected ];
     then
-        echo -e "Testing no file                    ${red}PASS${NC}";
+        echo -e "Testing no file                    ${green}PASS${NC}";
     else
-        echo -e "Testing no file                    ${green}FAIL${NC}";
+        echo -e "Testing no file                    ${red}FAIL, return code $value, expected $expected${NC}" ;
 fi
 
-./lexical file0 > ./test0.outputs/file0.output 2>&1;
+./parser file0 > ./test0.outputs/file0.output 2>&1;
 value=$?;
-if [ $value = 0 ];
+expected=0;
+if [ $value = $expected ];
     then
-        echo -e "Testing file0                      ${red}PASS${NC}";
+        echo -e "Testing file0                      ${green}PASS${NC}";
     else
-        echo -e "Testing file0                      ${green}FAIL{NC}";
+        echo -e "Testing file0                      ${red}FAIL, return code $value, expected $expected${NC}";
 fi
 
-./lexical file1 > test0.outputs/file1.output 2>&1;
+./parser file1 > test0.outputs/file1.output 2>&1;
 value=$?;
-if [ $value = 0 ];
+expected=0;
+if [ $value = $expected ];
     then
-        echo -e "Testing file1                      ${red}PASS${NC}";
+        echo -e "Testing file1                      ${green}PASS${NC}";
     else
-        echo -e "Testing file1                      ${green}FAIL{NC}";
+        echo -e "Testing file1                      ${red}FAIL, return code $value, expected $expected${NC}";
 fi
 
-./lexical file2 > test0.outputs/file2.output 2>&1;
+./parser file2 > test0.outputs/file2.output 2>&1;
 value=$?;
+expected=0;
 if [ $value = 0 ];
     then
-        echo -e "Testing file2                      ${red}PASS${NC}";
+        echo -e "Testing file2                      ${green}PASS${NC}";
     else
-        echo -e "Testing file2                      ${green}FAIL{NC}";
+        echo -e "Testing file2                      ${red}FAIL, return code $value, expected $expected${NC}";
 fi
 
-./lexical file3 > test0.outputs/file3.output 2>&1;
+./parser file3 > test0.outputs/file3.output 2>&1;
 value=$?;
-if [ $value = 0 ];
+expected=0;
+if [ $value = $expected ];
     then
-        echo -e "Testing file3                      ${red}PASS${NC}";
+        echo -e "Testing file3                      ${green}PASS${NC}";
     else
-        echo -e "Testing file3                      ${green}FAIL{NC}";
+        echo -e "Testing file3                      ${red}FAIL, return code $value, expected $expected${NC}";
 fi
 
-./lexical file4 > test0.outputs/file4.output 2>&1;
+./parser file4 > test0.outputs/file4.output 2>&1;
 value=$?;
-if [ $value = 0 ];
+expected=0;
+if [ $value = $expected ];
     then
-        echo -e "Testing file4                      ${red}PASS${NC}";
+        echo -e "Testing file4                      ${green}PASS${NC}";
     else
-        echo -e "Testing file4                      ${green}FAIL{NC}";
+        echo -e "Testing file4                      ${red}FAIL, return code $value, expected $expected${NC}";
 fi
 
-./lexical file5 > test0.outputs/file5.output 2>&1;
+./parser file5 > test0.outputs/file5.output 2>&1;
 value=$?;
-if [ $value = 0 ];
+expected=0;
+if [ $value = $expected ];
     then
-        echo -e "Testing file5                      ${red}PASS${NC}";
+        echo -e "Testing file5                      ${green}PASS${NC}";
     else
-        echo -e "Testing file5                      ${green}FAIL{NC}";
+        echo -e "Testing file5                      ${red}FAIL, return code $value, expected $expected${NC}";
+fi
+
+./parser file6 > test0.outputs/file6.output 2>&1;
+value=$?;
+expected=2;
+if [ $value = $expected ];
+    then
+        echo -e "Testing file6                      ${green}PASS${NC}";
+    else
+        echo -e "Testing file6                      ${red}FAIL, return code $value, expected $expected${NC}";
+fi
+
+./parser file7 > test0.outputs/file7.output 2>&1;
+value=$?;
+expected=3;
+if [ $value = $expected ];
+    then
+        echo -e "Testing file7                      ${green}PASS${NC}";
+    else
+        echo -e "Testing file7                      ${red}FAIL, return code $value, expected $expected${NC}";
+fi
+
+./parser file8 > test0.outputs/file8.output 2>&1;
+value=$?;
+expected=2;
+if [ $value = $expected ];
+    then
+        echo -e "Testing file8                      ${green}PASS${NC}";
+    else
+        echo -e "Testing file8                      ${red}FAIL, return code $value, expected $expected${NC}";
+fi
+
+./parser file9 > test0.outputs/file9.output 2>&1;
+value=$?;
+expected=3;
+if [ $value = $expected ];
+    then
+        echo -e "Testing file9                      ${green}PASS${NC}";
+    else
+        echo -e "Testing file9                      ${red}FAIL, return code $value, expected $expected${NC}";
+fi
+
+./parser file10 > test0.outputs/file10.output 2>&1;
+value=$?;
+expected=2;
+if [ $value = $expected ];
+    then
+        echo -e "Testing file10                     ${green}PASS${NC}";
+    else
+        echo -e "Testing file10                     ${red}FAIL, return code $value, expected $expected${NC}";
+fi
+
+./parser file11 > test0.outputs/file11.output 2>&1;
+value=$?;
+expected=2;
+if [ $value = $expected ];
+    then
+        echo -e "Testing file11                     ${green}PASS${NC}";
+    else
+        echo -e "Testing file11                     ${red}FAIL, return code $value, expected $expected${NC}";
 fi
 
 exit 0;
