7de0b5a20350a2904b630bd4856c1f11b09fc1d5
RPMcMurphy <8blink182@gmail.com>
2014-12-05 09:21:46 +0100

    Update struct.h

--- struct.h
+++ struct.h
@@ -2,7 +2,89 @@
 #define _STRUCT_H
 
 #include <stdio.h>
+#include <stdlib.h>#ifndef _STRUCT_H
+#define _STRUCT_H
+
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+
+typedef enum
+{
+	/*ADRES*/
+	I_LABEL,
+	I_RETURN,
+	I_CALL,
+	/*ARITMETIKA*/
+	I_ADD,
+	I_SUB,
+	I_MUL,
+	I_DIV,
+	I_MOD,
+	/*COMPARING*/
+	I_LESS_CMP,
+	I_GR_CMP,
+	I_LESS_EQUAL_CMP,
+	I_GR_EQUAL_CMP,
+	I_EQUAL_CMP,
+	I_NOT_EQUAL_CMP,
+
+I_ASSIGN,
+I_PRINT,
+I_READ
+//doplnit co treba
+
+}InstName;
+
+
+
+typedef struct Instruction
+{
+	InstName Iname;
+	struct Variable *a;
+	struct Variable *b;
+	void *res;
+}tInstruction;
+
+
+
+
+	/*************VALUEEEE********************/
+
+typedef struct
+{
+		int i;
+		double r;
+		char *s;
+		//string s;
+}VariableData;
+
+/*typedef enum
+{
+		VAR_Bool,
+		VAR_Int,
+		VAR_Real,
+		VAR_String,
+		VAR_Var
+}VariableType;*/
+
+
+
+typedef struct Variable{
+
+	VariableData data;
+	VariableType type;
+
+}Variable;
+
+
+
+
+
+#endif
+
 #include <stdint.h>
 #include <string.h>
 
