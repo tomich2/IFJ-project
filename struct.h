26d71f2f839a3a9756af0f4d8397dbe430963101
RPMcMurphy <8blink182@gmail.com>
2014-12-05 09:43:02 +0100

    Update struct.h

--- struct.h
+++ struct.h
@@ -6,6 +6,10 @@
 #include <stdint.h>
 #include <string.h>
 
+#ifndef TVART
+#define TVART
+typedef enum{tINTEGER, tSTRING, tREAL, tBOOLEAN, tERR=-1}T_vartype;
+#endif
 
 typedef enum
 {
@@ -71,85 +75,8 @@ typedef struct
 typedef struct Variable{
 
 	VariableData data;
-	VariableType type;
+	T_vartype type;
 
-}Variable;
-
-
-
-
-
-#endif
-
-#include <stdint.h>
-#include <string.h>
-
-
-typedef enum
-{
-	/*ADRES*/
-	I_LABEL,
-	I_RETURN,
-	I_CALL,
-	/*ARITMETIKA*/
-	I_ADD,
-	I_SUB,
-	I_MUL,
-	I_DIV,
-	I_MOD,
-	/*COMPARING*/
-	I_LESS_CMP,
-	I_GR_CMP,
-	I_LESS_EQUAL_CMP,
-	I_GR_EQUAL_CMP,
-	I_EQUAL_CMP,
-	I_NOT_EQUAL_CMP,
-	
-		
-//doplnit co treba
-
-}InstName;
-
-
-
-typedef struct Instruction
-{	
-	InstName Iname;
-	struct Variable *a;
-	struct Variable *b;
-	void *res;
-}tInstruction;
-
-	
-
-
-	/*************VALUEEEE********************/
-
-typedef struct 
-{
-		int i;
-		double r;
-		bool b;
-		char *s;
-		//string s;
-}VariableData;
-	 
-typedef enum 
-{
-		VAR_Bool,
-		VAR_Int,
-		VAR_Real,
-		VAR_String
-		VAR_Var		//ked bude typu VAR_Var tak v data.s bude nazov, nie obycajny string
-}VariableType;
-	 
-
-
-typedef struct Variable{
-
-	VariableData data;
-	VariableType type;
-	
 }Variable;
 
 
