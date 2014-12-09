69cfefda00b3881b34538b1190405f7ef4732b32
findo11 <fl.existenz@gmail.com>
2014-12-09 12:21:24 +0100

    Update interpret.h

--- interpret.h
+++ interpret.h
@@ -3,6 +3,8 @@
 
 #include "list.h"
 
-void interpretLoop(tListOfInstr *instList,t_varfunc_list *varList);
+void interpretLoop(tListOfInstr *instList,t_varfunc_list *varList,t_lablist *lablist);
+void print_my_string (char *str);
+void printchar(unsigned char theChar);
 
 #endif
