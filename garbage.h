72b294a29b9c03b537ba0e31e3a43133e5ea3a1c
Jamiroquai <jan.j.profant@gmail.com>
2014-12-12 16:45:03 +0100

    Update garbage.h

--- garbage.h
+++ garbage.h
@@ -1,9 +1,10 @@
 // IFJ14, projekt do predmetu IFJ pre 2BIT 2014/2015 //
+
 /////// Autor: Jan Profant
 ///////        Filip Badan
 ///////        Michal Chomo
 ///////        Tomas Chomo
-///////        Findo
+///////        Filip Listiak
 
 #ifndef _GARBAGE_H
 #define _GARBAGE_H
@@ -11,6 +12,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+
+// Subor garbage.h obsahuje deklaraciu funkcii a struktur potrebnych pre pracu s tzv. Garbage collectorom,
+// tento collector bol pouzity z dovodu, ze pri volani funkcii je potrebne dynamicky pracovat
+// s pamatou a nasledne ju aj transparentne uvolnit
 
 typedef struct TGarbage {
     struct TGarbage *ptr;
