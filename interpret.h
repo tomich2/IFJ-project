b50349fa41d96649c855a13ad158ec2d9584957e
Jamiroquai <jan.j.profant@gmail.com>
2014-12-12 22:19:30 +0100

    Update interpret.h

--- interpret.h
+++ interpret.h
@@ -6,5 +6,8 @@
 void interpretLoop(tListOfInstr *instList,t_varfunc_list *varList,t_lablist *lablist);
 void print_my_string (char *str);
 void printchar(unsigned char theChar);
+char * convert_my_string (char *str);
+char convertchar(unsigned char theChar);
+
 
 #endif
